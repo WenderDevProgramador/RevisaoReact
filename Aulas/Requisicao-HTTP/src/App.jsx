5f7ceb578e4763f3b71352d944733e17010808a4
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2026-06-07 19:03:12 -0300

    Avançando em req

--- Aulas/Requisicao-HTTP/src/App.jsx
+++ Aulas/Requisicao-HTTP/src/App.jsx
@@ -1,12 +1,95 @@
 
+import { useState, useEffect } from 'react'
+
+
 import './App.css'
 
+const url = 'http://localhost:3000/products'
+
 function App() {
-  
+
+  // Resgistrando produtos
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+
+    async function getData() {
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      console.log(data)
+
+      setProducts(data)
+    }
+
+    getData()
+
+  }, [])
+
+  // Envio de dados:
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price
+    };
+
+    const res = await fetch(url, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+
+    })
+
+    // Carregamento dinamico:
+
+    const addedProduct = await res.json()
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+  }
+
+
+
 
   return (
     <div className="App">
       <h1>HTTP React</h1>
+      <ul>
+        {products.map((product) =>
+          <li key={product.id}>{`${product.name} - R$ ${product.price}`}</li>
+        )}
+      </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label >
+
+            <span>
+              Nome:
+            </span>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+
+          <label >
+
+            <span>
+              Preço:
+            </span>
+            <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+          </label>
+
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
     </div>
   )
 }
